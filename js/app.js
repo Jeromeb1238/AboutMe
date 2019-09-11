913c71b1af25e58ca44fd855e88ec64a616841a9
Jerome Burns <jeromeb1238@hotmail.com>
2019-09-10 22:31:23 -0700

    Some content, style, script

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-
 'use strict'
 
 console.log("Hello")
 
 var name = prompt('What is your name?');
   console.log('Your name is ' + name);  
-  alert('Hello ' + name);
+  alert('Hello ' + name\.  I\'m going to ask you some questions that I talk about');
 
 var growUp = prompt('Did you grow up in Washington?');
 
@@ -13,7 +12,7 @@ growUp = growUp.toLocaleUpperCase();
 
 if(growUp == 'Y'|| growUp == 'YES'){
   console.log('User entered yes');
-  alert('Awesome you grew up in WA');
+  alert('Awesome\, you grew up in WA\, so did I');
 } 
 
   else if(growUp == 'N' || growUp == 'NO'){
@@ -26,3 +25,21 @@ if(growUp == 'Y'|| growUp == 'YES'){
     alert('Invalid answer');
 }
 
+var pet = prompt('Do you have a pet?');
+
+pet = pet.toLocaleUpperCase();
+
+if(pet == 'Y'|| pet == 'YES'){
+  console.log('User entered yes');
+  alert('Pets are great\. I have two dogs');
+} 
+
+  else if(pet == 'N' || pet == 'NO'){
+    console.log('User entered no');
+    alert('');
+  }
+
+  else{
+    console.log('Invalid answer');
+    alert('Invalid answer');
+}
